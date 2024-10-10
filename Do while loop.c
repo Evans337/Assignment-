2d5cc81439d812e05e3312903d088fbb6a532334
Evans337 <evansmutuma13858@gmail.com>
2024-10-10 17:45:26 +0300

    Create Do while loop.c

---
+++ Do while loop.c
@@ -0,0 +1,24 @@
+//program for loop user input 
+/*
+Name:evans_mutuma
+Ref:CT101/G/23738/24
+Date:11/11/2030
+*/
+#include<stdio.h>
+int main(){
+int start,sum,stop;
+printf("Enter when to start\n");
+scanf ("%d",& start);
+printf("Enter when to stop\n");
+scanf( "%d", &stop );
+do{
+printf ("%d\n",start);
+start++;
+sum +=start
+;
+
+}
+while( start<=stop);
+printf (" the sum is= %d\n",sum);
+return 0;
+}
